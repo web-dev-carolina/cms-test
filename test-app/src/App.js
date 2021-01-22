d1916c18d753f9af74c1f1159461e3caeda5561c
rushil-shah87 <54685822+rushil-shah87@users.noreply.github.com>
2021-01-22 17:35:35 -0500

    includes generic api for cms clients

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,17 +1,25 @@
-// import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import NavbarContainer from "./components/NavbarContainer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import { BrowserRouter } from 'react-router-dom';
-// import UserContext from "./context/UserContext.js";
-// import Axios from "axios";
+import Axios from "axios";
 
 function App() {
+
+  useEffect(() => {
+    async function fetchData() {
+      const reqBody = { project: "test-app" };
+      await Axios.get("http://localhost:9000/projects/connect", reqBody);
+    }
+    fetchData();
+  }, []);
+
   return (
-      <BrowserRouter>
-        <NavbarContainer> </NavbarContainer>
-      </BrowserRouter>
+    <BrowserRouter>
+      <NavbarContainer> </NavbarContainer>
+    </BrowserRouter>
 
   );
 }
